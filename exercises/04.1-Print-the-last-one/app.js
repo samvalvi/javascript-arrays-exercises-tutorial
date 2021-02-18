4796b215bd3d42b3eba48fec3b6ff42f1d3b7dfb
samvalvi <samuelvalerin@gmail.com>
2021-02-18 20:01:45 +0000

    exercise 05.3

--- exercises/04.1-Print-the-last-one/app.js
+++ exercises/04.1-Print-the-last-one/app.js
@@ -8,3 +8,5 @@ function generateRandomArray()
 var myStupidArray = generateRandomArray();
 
 //Your code here
+let theLastOne = myStupidArray[myStupidArray.length -1];
+console.log(theLastOne);
--- exercises/05.3-Add-items-to-array/app.js
+++ exercises/05.3-Add-items-to-array/app.js
@@ -2,4 +2,8 @@ var arr = [4,5,734,43,45];
 
 // Your code here, use the push function and the random function.
 
+for(let i = 0; i < 2; i++){
+    arr.push(Math.floor(Math.random()* 100))
+}
+
 console.log(arr);
