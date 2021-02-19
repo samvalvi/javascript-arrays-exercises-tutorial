25ae394bd973cbed97f37076122dcf49d10c825f
samvalvi <samuelvalerin@gmail.com>
2021-02-19 20:40:53 +0000

    exercise 21.3

--- exercises/21.3-Filter-list/app.js
+++ exercises/21.3-Filter-list/app.js
@@ -2,4 +2,10 @@ let names = ['Liam','Emma','Noah','Olivia','William','Ava','James','Isabella','L
 
 //declare your function here
 
-console.log(filterByName(names, 'am'));
+const filteringList = (arr, searchValue) => {
+    let values = arr.filter(name => name.toLowerCase().includes(searchValue.toLowerCase()));
+
+    return values;
+}
+
+console.log(filteringList(names, 'am'));
