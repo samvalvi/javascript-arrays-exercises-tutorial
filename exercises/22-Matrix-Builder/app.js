635e3a9bae6ccee2dafb92c688e9196e90cef336
samvalvi <samuelvalerin@gmail.com>
2021-02-20 03:15:24 +0000

    exercise 22

--- exercises/22-Matrix-Builder/app.js
+++ exercises/22-Matrix-Builder/app.js
@@ -1,5 +1,11 @@
 // Code goes here
+const matrixBuilder = num => {
+    let matrix = [];
+    
+    matrix = Array.from({length: 3}, () => Array.from({length: 3}, () => Math.floor(Math.random() * 2)));
 
+    return matrix;
+}
 
 
 // do not change anything from this line down
--- exercises/23-Parking-Lot/app.js
+++ exercises/23-Parking-Lot/app.js
@@ -3,4 +3,14 @@ let parking_state = [
   [0,0,0],
   [1,1,2]
 ]
+
+const getParkingLotState = () => {
+    let state = {totalSlots, availableSlots, occupiedSlots}
+
+
+
+
+    return totalSlots;
+}
+
 console.log(getParkingLotState(parking_state))
